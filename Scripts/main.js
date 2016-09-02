bc39c13f518173634ab93861ad246777ca011b33
seanloughran <seancloughran@gmail.com>
2016-09-02 12:21:03 -0700

    Files added, image added

---
+++ Scripts/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  $("#about").hide();
+  $("#homeBtn").addClass("current");
+
+  $(".navBtn").on("click", function() {
+    $(".navBtn").removeClass("current");
+    $("#main section").hide();
+  });
+
+  $("#homeBtn").on("click", function() {
+    $("#homeBtn").addClass("current");
+    $("#home").show();
+  });
+
+  $("#aboutBtn").on("click", function() {
+    $("#aboutBtn").addClass("current");
+    $("#about").show();
+  });
+
+  $("#newJoke").mouseover(function() {
+    $("#newJoke").css("background-color", "black");
+  });
+
+  $("#newJoke").mouseout(function() {
+    $("#newJoke").css("background-color", "#660000");
+  });
+
+  $("#newJoke").on("click", loadJoke);
+
+  function loadJoke() {
+    $.getJSON("http://api.icndb.com/jokes/random", function(json) {
+      $("#home p").html('"' + json.value.joke + '"');
+    });
+  };
+
+  loadJoke();
+});
